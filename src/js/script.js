713f47397245de84d2e1863f3a2e6fb89b08a7fd
Wiktor Ziejewski <wiktor.ziejewski>
2021-05-10 01:31:01 +0200

    Pizzeria

--- src/js/script.js
+++ src/js/script.js
@@ -594,6 +594,27 @@
       });
     }
   }
+  class Booking {
+    constructor(element) {
+      const thisBooking = this;
+      thisBooking.render(element);
+      thisBooking.initWidgets();
+    }
+    render(element) {
+      const thisBooking = this;
+  
+      thisBooking.dom = {};
+      thisBooking.dom.wrapper = element;
+      thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+      thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    }
+    initWidget() {
+      const thisBooking = this;
+      thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+      thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    }
+  }
+  
   const app = {
     initMenu: function () {
       const thisApp = this;
